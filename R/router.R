61cdac148afe1e72da27dc1bccf9fc717a286076
mpadge <mark.padgham@email.com>
2021-10-10 23:26:42 +0200

    lint R/router.R

--- R/router.R
+++ R/router.R
@@ -1,15 +1,15 @@
 #' conscious_route
-#' 
+#'
 #' Route between nominated start and end points along greatest proportional
 #' length of streets named after women rather than men.
-#' 
+#'
 #' @param net Street network with gender column, returned by
 #' \link{gender_streetnet} function.
 #' @param start Start point of route.
 #' @param stop End point of route.
 #' @export
-conscious_route <- function (net, start, stop)
-{
+conscious_route <- function (net, start, stop) {
+
     net_f <- net
     index <- which (net_f$female)
     net_f$d_weighted [index] <- net_f$d_weighted [index] / 10
@@ -47,8 +47,8 @@ conscious_route <- function (net, start, stop)
     list (p_stats = p_stats, paths = psf, points = start_end)
 }
 
-path_stats <- function (pmat, net)
-{
+path_stats <- function (pmat, net) {
+
     # get edge IDs for the path
     from <- pmat$id [-nrow (pmat)]
     to <- pmat$id [-1]
@@ -66,23 +66,23 @@ path_stats <- function (pmat, net)
 }
 
 #' gender_streetnet
-#' 
+#'
 #' Weight a street network via \pkg{dodgr} function `weight_streetnet`, adding
 #' additional information on gender of street names
-#' 
+#'
 #' @inheritParams get_gender
 #' @param net Street network in \pkg{sf} format
 #' @param wt_profile Type of weighting to be used; see `dodgr::weight_streetnet`
 #' for details.
-#' @return A weighted street network in \pkg{dodgr} format. 
+#' @return A weighted street network in \pkg{dodgr} format.
 #' @export
-gender_streetnet <- function (net, wt_profile = "foot", country = "de")
-{
+gender_streetnet <- function (net, wt_profile = "foot", country = "de") {
+
     g <- get_gender (net$name)
     g$gender [is.na (g$text)] <- NA_character_
 
-    if (tolower (country) == "de")
-    {
+    if (tolower (country) == "de") {
+
         # These can not be personal names:
         index <- which (substring (g$text, 1, 6) == "Alter" |
                         substring (g$text, 1, 3) == "Am" |
@@ -98,14 +98,15 @@ gender_streetnet <- function (net, wt_profile = "foot", country = "de")
                         grepl ("Heide$", g$text))
         g$gender [index] <- "NAME_NOT_FOUND"
     }
+
     net$gender <- g$gender
 
     netw <- dodgr::weight_streetnet (net, wt_profile = wt_profile)
     netw$gender <- "NAME_NOT_FOUND"
     id_m <- net$osm_id [which (net$gender == "IS_MALE" |
-                                  net$gender == "IS_MOSTLY_MALE")]
+                               net$gender == "IS_MOSTLY_MALE")]
     id_f <- net$osm_id [which (net$gender == "IS_FEMALE" |
-                                  net$gender == "IS_MOSTLY_FEMALE")]
+                               net$gender == "IS_MOSTLY_FEMALE")]
     netw$gender [which (netw$way_id %in% id_m)] <- "IS_MALE"
     netw$gender [which (netw$way_id %in% id_f)] <- "IS_FEMALE"
 
