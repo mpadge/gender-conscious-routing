f3e7b724be644ed513f4ee955fb7181206eb313a
mpadge <mark.padgham@email.com>
2021-10-12 17:10:15 +0200

    change gender-streetnet to work with both sf and sc
    Noting that this slows it down because the gender routines are now applied to every edge

--- R/gender-streetnet.R
+++ R/gender-streetnet.R
@@ -4,14 +4,17 @@
 #' additional information on gender of street names
 #'
 #' @inheritParams get_gender
-#' @param net Street network in \pkg{sf} format
+#' @param net Street network in \pkg{sf} or \pkg{sc} format
 #' @param wt_profile Type of weighting to be used; see `dodgr::weight_streetnet`
 #' for details.
 #' @return A weighted street network in \pkg{dodgr} format.
 #' @export
 gender_streetnet <- function (net, wt_profile = "foot", country = "de") {
 
-    g <- get_gender (net$name)
+    netw <- dodgr::weight_streetnet (net, wt_profile = wt_profile,
+                                     keep_cols = "name")
+
+    g <- get_gender (netw$name)
     g$gender [is.na (g$text)] <- NA_character_
 
     if (tolower (country) == "de") {
@@ -32,16 +35,11 @@ gender_streetnet <- function (net, wt_profile = "foot", country = "de") {
         g$gender [index] <- "NAME_NOT_FOUND"
     }
 
-    net$gender <- g$gender
-
-    netw <- dodgr::weight_streetnet (net, wt_profile = wt_profile)
     netw$gender <- "NAME_NOT_FOUND"
-    id_m <- net$osm_id [which (net$gender == "IS_MALE" |
-                               net$gender == "IS_MOSTLY_MALE")]
-    id_f <- net$osm_id [which (net$gender == "IS_FEMALE" |
-                               net$gender == "IS_MOSTLY_FEMALE")]
-    netw$gender [which (netw$way_id %in% id_m)] <- "IS_MALE"
-    netw$gender [which (netw$way_id %in% id_f)] <- "IS_FEMALE"
+    netw$gender [which (g$gender == "IS_MALE" |
+                        g$gender == "IS_MOSTLY_MALE")] <- "IS_MALE"
+    netw$gender [which (g$gender == "IS_FEMALE" |
+                        g$gender == "IS_MOSTLY_FEMALE")] <- "IS_FEMALE"
 
     netw$female <- FALSE
     netw$female [which (netw$gender == "IS_FEMALE")] <- TRUE
