54b9a079e3f9f8feac9de7e5d4a6987df7d19f4e
mpadge <mark.padgham@email.com>
2022-04-21 15:08:19 +0200

    add gcr_city code

--- R/analyse-city.R
+++ R/analyse-city.R
@@ -9,4 +9,76 @@
 #' all pairs for the entire network.
 #' @export
 gcr_city <- function (net, wt_profile = "foot", n = NULL) {
+
+    dodgr::dodgr_cache_off ()
+
+    net <- readRDS (f) |>
+        gender_streetnet (wt_profile = wt_profile) |>
+        dodgr::dodgr_contract_graph ()
+    net$edge_type <- net$gender
+    v <- dodgr::dodgr_vertices (net)
+
+    if (!is.null (n)) {
+        from <- to <- sample (v$id, size = n)
+    } else {
+        from <- to <- v$id
+    }
+
+    d0 <- dodgr::dodgr_dists_categorical (net, from = from, to = to,
+                                          proportions_only = TRUE)
+    d0 <- c (
+        d0,
+        "IS_A_NAME" = sum (d0 [1:2]),
+        "IS_FEMALE_RAW" =
+            length (which (net$edge_type == "IS_FEMALE")) / nrow (net),
+        "IS_MALE_RAW" =
+            length (which (net$edge_type == "IS_MALE")) / nrow (net))
+    names (d0) [names (d0) == "NAME_NOT_FOUND"] <- "NOT_A_NAME"
+    d0 <- c (
+        d0,
+        "NOT_A_NAME_RAW" = 1 - sum (d0 [5:6]),
+        "IS_A_NAME_RAW" = sum (d0 [5:6]))
+
+    net$edge_type <- net$highway
+
+    if (wt_profile == "foot") {
+        types <- c (
+            "bridleway",
+            "track",
+            "path",
+            "living_street",
+            "steps",
+            "pedestrian")
+        this_category <- "pedestrian"
+    } else {
+        types <- c (
+            "bridleway",
+            "track",
+            "path",
+            "living_street",
+            "pedestrian",
+            "cycleway")
+        this_category <- "bicycle"
+    }
+
+    types <- paste0 ("^", paste0 (types, collapse = "|^"))
+    net$edge_type [grep (types, net$edge_type)] <- this_category
+    net$edge_type [net$edge_type == "unclassified"] <- NA_character_
+    net$edge_type [net$edge_type != this_category &
+                   !is.na (net$edge_type)] <- "vehicular"
+
+    d1 <- dodgr::dodgr_dists_categorical (net, from = from, to = to,
+                                          proportions_only = TRUE)
+    d1 <- c (d1,
+             length (which (net$edge_type == this_category)) / nrow (net),
+             length (which (net$edge_type == "vehicular")) / nrow (net))
+    names (d1) [3:4] <- paste0 (c (this_category, "vehicular"), "_RAW")
+
+    names (d1) <- gsub ("vehicular",
+                        paste0 ("vehicular_", wt_profile), names (d1))
+
+    ret <- c (d0, d1)
+    data.frame (wt_profile = wt_profile,
+                category = names (ret),
+                proportion = as.numeric (ret))
 }
