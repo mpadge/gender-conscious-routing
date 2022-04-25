c57386145ff65231cf60d84531bd986df8f8a8c5
mpadge <mark.padgham@email.com>
2022-04-25 20:51:44 +0200

    add wikidata_gender fn for #12

--- R/gender-streetnet.R
+++ R/gender-streetnet.R
@@ -43,8 +43,49 @@ gender_streetnet <- function (net, wt_profile = "foot", country = "de") {
     netw$gender [which (g$gender == "IS_FEMALE" |
                         g$gender == "IS_MOSTLY_FEMALE")] <- "IS_FEMALE"
 
+    netw <- wikidata_gender (netw)
+
     netw$female <- FALSE
     netw$female [which (netw$gender == "IS_FEMALE")] <- TRUE
 
     return (netw)
+}
+
+wikidata_gender <- function (net) {
+
+    wiki <- unique (net$`name:etymology:wikidata`)
+    wiki <- wiki [which (!is.na (wiki))]
+
+    wd <- paste0 (paste0 ("wd:", wiki), collapse = " ")
+    qry <- paste0 ('
+        SELECT DISTINCT ?person ?personLabel ?genderLabel WHERE {
+          VALUES ?person {', wd, '
+          }
+        ?person wdt:P21 ?gender;
+        SERVICE wikibase:label {
+            bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
+    }')
+    qry <- gsub ("\\s+", "%20", gsub ("^\\s?", "", qry))
+
+    u <- sprintf ("https://query.wikidata.org/sparql?format=json&query=%s", qry)
+    res <- jsonlite::fromJSON (u)
+    res <- res$results$bindings$genderLabel$value
+
+    # https://www.wikidata.org/wiki/Property:P21
+    gender_map <- rbind (
+        c ("male", "Q6581097"),
+        c ("female", "Q6581072"),
+        c ("intersex", "Q1097630"),
+        c ("transgender", "emale: Q1052281"),
+        c ("transgender", "ale: Q2449503")
+        )
+
+    gender <- gender_map [match (res, gender_map [, 2]), 1]
+    gender <- paste0 ("IS_", toupper (gender))
+
+    index <- which (net$`name:etymology:wikidata` %in% wiki)
+    index2 <- match (net$`name:etymology:wikidata` [index], wiki)
+    net$gender [index] <- gender [index2]
+
+    return (net)
 }
